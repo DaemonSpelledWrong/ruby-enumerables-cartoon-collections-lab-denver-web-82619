ff142ede48d3f95eb513d51cabac5050d17db2c6
Damon Self <damongself@gmail.com>
2019-08-16 18:08:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,9 +12,9 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if shared = array & cheese_types
-    
-  else
+  if shared = array & cheese_types = []
     return nil
+  else
+    return shard
   end
 end

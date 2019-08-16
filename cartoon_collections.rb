2109075158a8556f2074a1485a272184879019b4
Damon Self <damongself@gmail.com>
2019-08-16 18:09:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,9 +12,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if shared = array & cheese_types = []
-    return nil
-  else
-    return shared
-  end
+  array.first()
 end

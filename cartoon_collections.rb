f5eb62fc9c26bb73440c553d941ed9501b29e33d
Damon Self <damongself@gmail.com>
2019-08-16 17:33:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.collect {|x| puts "#{array.index(x) + 1} #{x}" }
 end
 
 def summon_captain_planet# code an argument here
